fe01656ea2c8de9097798b7f5b3e419aca500c2a
dkmsfire <leozxc2009@gmail.com>
2019-06-10 11:20:41 +0800

    update hw6

--- hw6/hw6.R
+++ hw6/hw6.R
@@ -13,4 +13,30 @@ train_season = na.seasplit(train_ts, algorithm = "mean")
 model = auto.arima(train_season)
 plot(forecast(model, h = 176))
 
-#56
+#56 yt = 25 + 0.8 * yt-1 + et
+##a
+xt <- arima.sim(list(order = c(1,0,0), ar = 0.8), n = 100)
+yt <- xt + 25   
+plot.ts(yt)
+model = auto.arima(yt)
+##b
+x = rnorm(100, mean = 0, sd = 1)
+yt = yt + x
+plot.ts(yt)
+##c
+yt.ts = ts(yt)
+acf(yt.ts)
+pacf(yt.ts)
+
+
+###60
+##a theoretical acf and pacf
+acf_t = ARMAacf(ar=c(0.6, 0.25), lag.max=50)
+pacf_t = ARMAacf(ar=c(0.6, 0.25), lag.max=50, pacf = TRUE)
+write.csv(acf_t, file = "acf_t.csv")
+write.csv(pacf_t, file = "pacf_t.csv")
+##b AR(1)
+acf_s = ARMAacf(ar=c(0.6), lag.max=50)
+pacf_s = ARMAacf(ar=c(0.6), lag.max=50, pacf = TRUE)
+write.csv(acf_s, file = "acf_s.csv")
+write.csv(pacf_s, file = "pacf_s.csv")
